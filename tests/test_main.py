ae2800010433d05b38bc824fd4968c159ba0419c
Juanse <jtaborda579@soyudemedellin.edu.co>
2025-02-19 18:30:58 -0500

    add new requirements

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,4 +11,33 @@ def test_caso_36_cuotas():
     assert total_interes == 134726.53
 
 def test_caso_24_cuotas():
-    pass
+    tarjeta_de_credito: TarjetaDeCredito = TarjetaDeCredito(3.4, 25000)
+    compra: Compra = Compra(850000, 3.4, 24)
+    tarjeta_de_credito.registrar_compra(compra)
+    total_interes = tarjeta_de_credito.calcular_total_interes()
+    assert total_interes == 407059.97
+
+def test_caso_48_cuotas():
+    tarjeta_de_credito: TarjetaDeCredito = TarjetaDeCredito(0, 2000000)
+    compra: Compra = Compra(480000, 0, 48)
+    tarjeta_de_credito.registrar_compra(compra)
+    total_interes = tarjeta_de_credito.calcular_total_interes()
+    assert total_interes == 0
+
+
+def test_caso_60_cuotas():
+    tarjeta_de_credito: TarjetaDeCredito = TarjetaDeCredito(12.5, 30000)
+    compra: Compra = Compra(50000, 12.5, 60)
+    tarjeta_de_credito.registrar_compra(compra)
+    total_interes = tarjeta_de_credito.calcular_total_interes()
+    assert total_interes == "!Error!: La tasa de interés supera la tasa de usura!"
+
+
+def test_caso_1_cuota():
+    tarjeta_de_credito: TarjetaDeCredito = TarjetaDeCredito(2.4, 200)
+    compra: Compra = Compra(90000, 2.4, 1)
+    tarjeta_de_credito.registrar_compra(compra)
+    total_interes = tarjeta_de_credito.calcular_total_interes()
+    assert total_interes == 0
+
+
